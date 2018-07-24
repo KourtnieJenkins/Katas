93402d28a0cf0f62aae1f08cf6af2104a98fa6c1
Kourtnie Jenkins <31581933+KourtnieJenkins@users.noreply.github.com>
2018-07-24 10:59:03 -0400

    Added new Tic Tac Toe Kata project.

---
+++ TicTacToe3x3Kata/TicTacToe3x3Kata/TicTacToeGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe3x3Kata
+{
+    class TicTacToeGame
+    {
+    }
+}
